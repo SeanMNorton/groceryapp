7706c69019fbcb8fde38ae2a1db4ad124ec84cc9
jonnorstrom <jon.norstrom@gmail.com>
2016-04-25 23:35:55 -0500

    ALL associations and active record methods set up and working

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ActiveRecord::Base
   # Remember to create a migration!
-  has_many :ingredients # good
-  belongs_to :author, class_name: "User" # good
+  has_many :ingredients
+  has_many :items, through: :ingredients, source: :item # good
+  belongs_to :author, class_name: "User", foreign_key: :user_id# good
 end
